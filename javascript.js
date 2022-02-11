1d3005b4334288f74a0d8f6daac9db1f18dfe612
Tyler Durden <tylerdurden1412@gmail.com>
2022-02-11 22:36:56 +0200

    player selection ready

--- javascript.js
+++ javascript.js
@@ -3,4 +3,27 @@ function computerPlay() {
     num = Math.floor(Math.random() * 3);
     return computerChoice[num];
 }
-console.log(computerPlay());
+function playerSelection() {
+    let rightChoice = false;
+    while (rightChoice === false){
+        playerChoice = window.prompt("Choose (1 for Rock) (2 for Paper) (3 for Scissors)");
+        playerChoice = parseInt(playerChoice);
+        if (playerChoice === 1) {
+            rightChoice = true;
+            return "rock"  
+        }
+        else if (playerChoice === 2) {
+            return "paper"
+            rightChoice = true;
+        }
+        else if (playerChoice === 3) {
+            return "scissors"
+            rightChoice = true;
+        }
+        else {
+            alert("Pick Again")
+        }
+    }
+}
+
+
