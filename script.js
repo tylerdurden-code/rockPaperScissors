872415bef1fe11e1cf80702f3d8333fd6fd4a539
Tyler Durden <tylerdurden1412@gmail.com>
2022-02-20 15:01:31 +0200

    add score and currenlty playing features

--- script.js
+++ script.js
@@ -5,59 +5,63 @@ function computerPlay() {
 }
 function playRound(p,c) {
     if (p === c) {
-        console.log("Its a tie");
+        h3.innerHTML = "Its a tie";
         computerScore += 1;
         playerScore += 1
     }
     else if (p === "rock" && c === "paper") {
-        console.log("You lost! Paper wins Rock!");
+        h3.innerHTML =  "You lost! Paper wins Rock!";
         computerScore += 1;
     }
     else if (p === "rock" && c === "scissors") {
-        console.log("You won! Rock beats Scissors");
+        h3.innerHTML =  "You won! Rock beats Scissors";
         playerScore += 1;
     }
     else if (p === "paper" && c === "rock") {
-        console.log("You won! Paper beats Rock");
+        h3.innerHTML =  "You won! Paper beats Rock";
         playerScore += 1;
     }
     else if (p === "paper" && c ==="scissors") {
-        console.log("You lost! Paper loses to Scissors");
+        h3.innerHTML = "You lost! Paper loses to Scissors";
         computerScore += 1;
     }
     else if (p === "scissors" && c === "rock") {
-        console.log("You lost! Scissors loses to Rock");
+        h3.innerHTML = "You lost! Scissors loses to Rock";
         computerScore += 1;
     }
     else if (p === "scissors" && c === "paper") {
-        console.log("You won! Scissors beats paper");
+        h3.innerHTML = "You won! Scissors beats paper";
         playerScore += 1;
     }
 }
 function game(usrInput) {
+    h1.innerHTML = "Playing....";
     playRound(usrInput,computerPlay())
-    div.innerHTML = `Your Score = ${playerScore}   PC Score = ${computerScore}`;
+    div.innerHTML = `Your Score = ${playerScore}   PC Score = ${computerScore}`;   
     if (playerScore === 3 || computerScore === 3) {
         if (playerScore > computerScore) {
             h1.innerHTML = "GAME FINISHED!!! YOU WON"
             playerScore = 0;
             computerScore = 0;
+            generalPlayerScore++;
         }
         else if (playerScore < computerScore){
-            console.log("GAME FINISHED!!! YOU LOST");
+            h1.innerHTML = "GAME FINISHED!!! YOU LOST";
             playerScore = 0;
             computerScore = 0;
+            generalComputerScore++;
         }
         else {
-            console.log("GAME FINISHED!!! ITS A TIE");
+            h1.innerHTML = "GAME FINISHED!!! ITS A TIE";
             playerScore = 0;
             computerScore = 0;
-        }
+            generalPlayerScore++;
+            generalPlayerScore++;
+        
         }
     }
-    if (playerScore === 0 && computerScore ===0) {
-        h1.innerHTML = "playing";
-    }
+    h1_0.innerHTML = `Your total score = ${generalPlayerScore} <br> PC total score = ${generalComputerScore}`;
+}
 
 
 
@@ -72,16 +76,22 @@ function game(usrInput) {
 
 var playerScore = 0;
 var computerScore = 0;
+var generalPlayerScore = 0;
+var generalComputerScore = 0;
 const btnRock = document.getElementById("btnRock");
 const btnPaper = document.getElementById("btnPaper");
 const btnScissors = document.getElementById("btnScissors");
 const body = document.querySelector('body');
+const h3 = document.createElement('h3')
 const h1 = document.createElement('h1')
 const div = document.createElement('div')
+const h1_0 = document.createElement('h1')
 h1.innerHTML = "playing"
 
 body.appendChild(div);
-body.appendChild(h1)
+body.appendChild(h3);
+body.appendChild(h1);
+body.appendChild(h1_0);
 
 
 btnRock.addEventListener('click',function () {
